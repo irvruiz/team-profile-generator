9b1a90f2bb24db18e8b646110b037b08a9b12514
Irvin <irvinruiz44@yahoo.com>
2022-02-20 12:23:45 -0700

    added js

---
+++ index.js
@@ -0,0 +1,51 @@
+const inqurier = require("inquirer");
+const fs = require("fs");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const { messageParent } = require("jest-worker");
+const { data } = require("browserslist");
+const { array } = require("yargs");
+var engineerarr = [];
+var managerArray = [];
+var internArray = [];
+var employeeArray = [];
+var engineerArray = [];
+var htmlCode;
+var endofHTML;
+
+promptManager();
+
+function promptManager() {
+  inqurier
+    .prompt([
+      {
+        type: "input",
+        message: "what is your name",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "what is your id",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "what is your email",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "what is your office number",
+        name: "officeNumber",
+      },
+    ])
+    .then((data) => {
+      console.log("here");
+      managerArray.push(
+        new Manager(data.name, data.id, data.email, data.officeNumber)
+      );
+      selectRole();
+    });
+}
