4708583de654610bf8bdb70d6bc829c7f3515750
Irvin <irvinruiz44@yahoo.com>
2022-02-20 12:59:58 -0700

    added js

--- index.js
+++ index.js
@@ -111,31 +111,75 @@ function promptEngineer() {
     });
 }
 function selectRole() {
-    inqurier.prompt([
-        {
-            type: "list",
-            choices: ["Intern", "Engineer", "Finishing building your team"],
-            message: "Pick a role",
-            name: "role"
-        }
+  inqurier
+    .prompt([
+      {
+        type: "list",
+        choices: ["Intern", "Engineer", "Finishing building your team"],
+        message: "Pick a role",
+        name: "role",
+      },
     ])
     .then((data) => {
-        if (data.role === "Engineer") {
-            promptEngineer();
-        }
-        if (data.role === "Intern") {
-            promptIntern();
-        }
-        if (data.role === "Finishing building your team") {
-            GenerateManager(managerArray);
-            //engineer cards
-            GenerateEngineer(engineerArray);
-            GenerateIntern(internArray);
-            renderHTML()
-            init()
+      if (data.role === "Engineer") {
+        promptEngineer();
+      }
+      if (data.role === "Intern") {
+        promptIntern();
+      }
+      if (data.role === "Finishing building your team") {
+        GenerateManager(managerArray);
+        //engineer cards
+        GenerateEngineer(engineerArray);
+        GenerateIntern(internArray);
+        renderHTML();
+        init();
 
-            console.log(renderHTML());
+        console.log(renderHTML());
+      }
+    });
+}
+function renderHTML() {
+  htmlCode = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+       
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="./style.css" />
+        
+        <title>Document</title>
+    </head>
+    <body>
+        <div class="card" style="width: 18rem;">`;
 
-        }
-     }) 
+  htmlCode += employeeArrayReturn();
+  htmlCode += `</div >
+    </body>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+   </html> `;
+  return htmlCode;
+}
+function GenerateManager(managerArray) {
+  temp = `<div class="card" style="width: 18rem;">
+      <!-- <img src="..." class="card-img-top" alt="..."> -->
+      <div class="card-body">
+        <h5 class="card-title">${managerArray[0].getName()}</h5>
+        <p class="card-text">Manager</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">${managerArray[0].getId()}</li>
+        <li class="list-group-item">${managerArray[0].getEmail()}</li>
+        <li class="list-group-item">${managerArray[0].getOfficeNumber()}</li>
+      </ul>
+      <!-- <div class="card-body">
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+      </div> -->
+    </div>`;
+  employeeArray.push(temp);
 }
