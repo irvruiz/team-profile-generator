fd3a74ac3d454e2d02d36d14c423e06ccfb27689
Irvin <irvinruiz44@yahoo.com>
2022-02-20 12:30:37 -0700

    added js

--- index.js
+++ index.js
@@ -49,3 +49,33 @@ function promptManager() {
       selectRole();
     });
 }
+function promptIntern() {
+  inqurier
+    .prompt([
+      {
+        type: "input",
+        message: "what is your name",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "what is your id",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "what is your email",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "what is your school",
+        name: "school",
+      },
+    ])
+    .then((data) => {
+      console.log("here3");
+      internArray.push(new Intern(data.name, data.id, data.email, data.school));
+      selectRole();
+    });
+}
